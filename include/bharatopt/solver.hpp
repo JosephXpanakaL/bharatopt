90a7f02799d3b462a2a9c1e67c7d6e2185f70529
JxP <jxp400900@gmail.com>
2026-09-19 23:21:56 +0530

    feat: add QP model support

--- include/bharatopt/solver.hpp
+++ include/bharatopt/solver.hpp
@@ -5,8 +5,9 @@ class BharatOptSolverCore {
 public:
   SolverResult solve_lp(const LPModel& model, const SolverOptions& options = {});
   SolverResult solve_milp(const LPModel& model, const SolverOptions& options = {});
+  SolverResult solve_qp(const QPModel& model, const SolverOptions& options = {});
   SolverResult solve(const LPModel& model, const SolverOptions& options = {}) {
-    return model.has_integer_variables() ? solve_milp(model, options) : solve_lp(model, options);
+    return model.has_integer_variables()?solve_milp(model,options):solve_lp(model,options);
   }
 };
 }
