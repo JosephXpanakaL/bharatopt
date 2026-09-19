167ed7720c7fb033859ceaf860d80a36b60393a3
JxP <jxp400900@gmail.com>
2026-09-19 23:28:00 +0530

    feat: establish persistent CUDA solver interface

--- src/cuda_stub.cpp
+++ src/cuda_stub.cpp
@@ -1,7 +1,13 @@
 #include "bharatopt/cuda_backend.hpp"
+
 namespace bharatopt::cuda_backend {
-bool cuda_available(){return false;}
-std::string cuda_device_name(){return "disabled";}
-bool spmv(const SparseMatrixCSR&,const std::vector<double>&,std::vector<double>&){return false;}
-bool project_box(std::vector<double>&,const std::vector<double>&,const std::vector<double>&){return false;}
+
+bool cuda_available(){ return false; }
+std::string cuda_device_name(){ return "disabled"; }
+
+struct CudaPdhgSolver::Impl {};
+CudaPdhgSolver::CudaPdhgSolver(): impl_(new Impl{}) {}
+CudaPdhgSolver::~CudaPdhgSolver(){ delete impl_; }
+bool CudaPdhgSolver::solve(const LPModel&, const SolverOptions&, SolverResult&){ return false; }
+
 }
