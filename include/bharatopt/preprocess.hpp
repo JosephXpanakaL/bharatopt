6181b0909f0418ae0789812dbbd04f5915580c9d
JxP <jxp400900@gmail.com>
2026-09-19 23:30:31 +0530

    feat: add safe LP presolve and reversible Ruiz scaling

---
+++ include/bharatopt/preprocess.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "bharatopt/model.hpp"
+namespace bharatopt {
+struct PreprocessResult {
+  LPModel model;
+  std::vector<double> column_scale;
+  bool feasible{true};
+  std::string message;
+};
+PreprocessResult preprocess_lp(const LPModel& input, int scaling_passes = 5);
+void recover_primal(const std::vector<double>& scaled_x, const std::vector<double>& column_scale, std::vector<double>& x);
+}
